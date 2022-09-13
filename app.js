c759fb67307fadd4cb941380ce774ade96039e25
bekasturua <beqasturua3@gmail.com>
2022-09-13 10:18:53 +0400

    fix: change errors

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
-app.use(shopRouter)
+app.use(shopRoutes);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  console.log("In the another middleware!");
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-app.post("/product", (req, res, next) => {
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
   res.redirect("/");
 });
 
