2f25c278ff515a84f4852e20dde07f0503f41daf
bekasturua <beqasturua3@gmail.com>
2022-09-13 14:07:31 +0400

    feat: add product logic with pug

--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,13 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
-
-const router = express.Router();
 const adminData = require("./admin");
 
+const router = express.Router();
+
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
