18d305407f1b85efc6ee9c2f7fbc8c5a25cbf7af
Beka Sturua <beqasturua3@gmail.com>
2022-09-25 13:53:28 +0400

    change code for mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", null, {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://beqa3377:uht9HWVMxrKmDW2D@cluster0.t7jdho6.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
